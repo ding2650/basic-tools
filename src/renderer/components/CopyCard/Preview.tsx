de44a0c7dac05eb3e1a09cdfcbe0944e7f7bd865
Dinglh <1808656324@qq.com>
2022-11-24 17:15:12 +0800

    Add mask of card

--- src/renderer/components/CopyCard/Preview.tsx
+++ src/renderer/components/CopyCard/Preview.tsx
@@ -4,13 +4,21 @@ import moment from 'moment';
 interface PreviewProps {
   item: CopyCardProps;
 }
+
+const maskFormat = (value: string) => {
+  const len = value.length;
+  if (len < 5) {
+    return `${value.substring(0, 1)}****${value.substring(3, len)}`;
+  }
+  return `${value.substring(0, 2)}****${value.substring(len - 2, len)}`;
+};
 const Preview = (props: PreviewProps) => {
   const { value, updateDate } = props.item;
 
   return (
     <div>
       <div className="card-item-value">
-        <span>{value}</span>
+        <span>{maskFormat(value)}</span>
       </div>
       <span className="card-item-tag">
         {moment(updateDate).format('YYYY年MM月DD日 HH:mm:ss')}
